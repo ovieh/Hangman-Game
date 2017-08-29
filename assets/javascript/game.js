e4639e1206d4d649c9efa81f65ecafb908adf294
Ovieh <ovieh.mosley@gmail.com>
2017-08-29 16:47:10 -0400

    Working letter checking and underscore replacement.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,49 @@
 var pies = ['rhubarb', 'pecan', 'apple', 'chocolate cream', 'banana cream', 'shoo fly'];
 var randomPick = pies[Math.floor(Math.random() * pies.length)];
 var wins = 0;
-var lettersRemaining = 0;
-var dashes = "";
+var lettersRemaining = randomPick.length;
+var dashArray = [];
 
 
 console.log(randomPick);
 
 //So i need to figure out the length of each string in the array, then
 
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-   // console.log(lettersRemaining);
+// console.log(lettersRemaining);
 
-    for (var i = 0; i < pies.length; i++) {
-        lettersRemaining = pies[i].length;
-        for(var j = 0; j < pies[i].length; j++) {
-            if(userGuess===pies[i].charAt(j)) {
-                console.log(pies[i].charAt(j));
-            }            
-            else{
-            }
+function letterChecker(word) {
+    for (var i = 0; i < randomPick.length; i++) {
+        if (word === randomPick.charAt(i)) {
+            // console.log(randomPick.charAt(i));
+            dashArray[i] = randomPick.charAt(i);
+        } else {
+
         }
     }
+}
+
+function createDashes(word) {
+    for (var i = 0; i < randomPick.length; i++) {
+        dashArray.push("_");
+    }
+}
+
+createDashes(randomPick);
+console.log(dashArray);
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
+    letterChecker(userGuess);
+    console.log(dashArray);
+    
+
+
 
     var html =
         "<p>You chose: " + randomPick + "</p>" +
-        "<p>You pressed: " + userGuess + "</p>" +
-        "<p>" + dashes + "</p>";
+        "<p>You pressed: " + userGuess + "</p>"; // +
+        // "<p>" + dashArray + "</p>";
 
     document.querySelector("#game").innerHTML = html;
 }
