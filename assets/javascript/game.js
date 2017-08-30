c5315ae82e7a40ea4b87136ff0650c1721c3c4a1
Ovieh <ovieh.mosley@gmail.com>
2017-08-30 14:49:46 -0400

    Push wrong guesses into array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,9 @@ var randomPick = pies[Math.floor(Math.random() * pies.length)];
 var wins = 0;
 var lettersRemaining = randomPick.length;
 var dashArray = [];
-
+var guessArray = [];
+var guessCount = 0;
+var placeHolder = "";
 
 console.log(randomPick);
 
@@ -11,15 +13,21 @@ console.log(randomPick);
 
 // console.log(lettersRemaining);
 
-function letterChecker(word) {
-    for (var i = 0; i < randomPick.length; i++) {
-        if (word === randomPick.charAt(i)) {
-            // console.log(randomPick.charAt(i));
-            dashArray[i] = randomPick.charAt(i);
-        } else {
+// while (guessCount >= l)
+function letterChecker(letter) {
+    if (randomPick.indexOf(letter) === -1) {
+        guessArray.push(letter);
+    } else {
+        for (var i = 0; i < randomPick.length; i++) {
+            if (letter === randomPick.charAt(i)) {
+                // console.log(randomPick.charAt(i));
+                dashArray[i] = randomPick.charAt(i);
+            }
 
         }
     }
+    placeHolder = dashArray.join(" ");
+
 }
 
 function createDashes(word) {
@@ -28,22 +36,32 @@ function createDashes(word) {
     }
 }
 
+// function letterGuessed(word){
+//     for(var i=0; i<letter.length;i++){
+//         if()
+//     }
+// }
+
 createDashes(randomPick);
-console.log(dashArray);
+//console.log(dashArray);
+console.log(placeHolder);
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
     letterChecker(userGuess);
-    console.log(dashArray);
-    
+    // console.log(dashArray);
+    console.log(placeHolder);
+    //     guessCount++;
+    // console.log(lettersGuessed);
+
 
 
 
     var html =
         "<p>You chose: " + randomPick + "</p>" +
         "<p>You pressed: " + userGuess + "</p>"; // +
-        // "<p>" + dashArray + "</p>";
+    // "<p>" + dashArray + "</p>";
 
     document.querySelector("#game").innerHTML = html;
 }
